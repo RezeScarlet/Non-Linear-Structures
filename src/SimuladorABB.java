5fdb1b2713b34f603a8c8199e83b27352affdced
Aloísio M. Lingo Filho <60991374+laosioor@users.noreply.github.com>
2024-11-06 11:08:32 -0300

    fix Centro

--- src/SimuladorABB.java
+++ src/SimuladorABB.java
@@ -1,4 +1,6 @@
 
+import java.awt.Color;
+
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
@@ -39,26 +41,34 @@ public class SimuladorABB extends EngineFrame {
     arvore.put(1, "um");
     arvore.put(3, "três");
     nos = arvore.coletarParaDesenho();
+
     margemCima = 100;
     margemEsquerda = 50;
     raio = 20;
     espacamento = 50;
+//    for (ArvoreBinariaBusca.Node<Integer, String> no : nos) {
+//      no.centro = new Vector2(espacamento * no.ranque + margemEsquerda, espacamento * no.nivel + margemCima);
+//    }
+atualizarCentro(nos, espacamento);
   }
 
   @Override
   public void update() {
-
+//    for (ArvoreBinariaBusca.Node<Integer, String> no : nos) {
+//      no.centro = new Vector2(espacamento * no.ranque + margemEsquerda, espacamento * no.nivel + margemCima);
+//    }
+    atualizarCentro(nos, espacamento);
     Vector2 mousePos = getMousePositionPoint();
 
     if (isMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+//      for (ArvoreBinariaBusca.Node<Integer, String> no : nos) {
+//        no.centro = new Vector2(espacamento * no.ranque + margemEsquerda, espacamento * no.nivel + margemCima);
+//      }
+atualizarCentro(nos, espacamento);
 
       for (ArvoreBinariaBusca.Node<Integer, String> no : nos) {
 
-        Vector2 centro = new Vector2(
-            espacamento * no.ranque + margemEsquerda,
-            espacamento * no.nivel + margemCima);
-
-        if (CollisionUtils.checkCollisionPointCircle(mousePos, centro, raio)) {
+        if (CollisionUtils.checkCollisionPointCircle(mousePos, no.centro, raio)) {
           SwingUtilities.invokeLater(() -> {
             int opcao = JOptionPane.showConfirmDialog(
                 this,
@@ -70,24 +80,43 @@ public class SimuladorABB extends EngineFrame {
               nos = arvore.coletarParaDesenho();
             }
           });
+
         }
 
       }
 
     }
-
   }
 
   @Override
   public void draw() {
     for (ArvoreBinariaBusca.Node<Integer, String> no : nos) {
-      desenharNo(no, espacamento, espacamento);
+      desenharNo(no);
+      desenharSetas(no);
     }
   }
 
-  private void desenharNo(ArvoreBinariaBusca.Node<Integer, String> no, int espHorizontal, int espVertical) {
-    fillCircle(espHorizontal * no.ranque + margemEsquerda, espVertical * no.nivel + margemCima, raio, no.cor);
-    drawCircle(espHorizontal * no.ranque + margemEsquerda, espVertical * no.nivel + margemCima, raio, BLACK);
+  private void desenharNo(ArvoreBinariaBusca.Node<Integer, String> no) {
+    fillCircle(no.centro.x, no.centro.y, raio, no.cor);
+    drawCircle(no.centro.x, no.centro.y, raio, BLACK);
+  }
+
+  private void desenharSetas(ArvoreBinariaBusca.Node<Integer, String> no) {
+    if (no.left != null) {
+      drawLine(new Vector2(no.centro.x - raio, no.centro.y), new Vector2(no.left.centro.x, no.left.centro.y - raio),
+          Color.BLACK);
+    }
+
+    if (no.right != null) {
+      drawLine(new Vector2(no.centro.x + raio, no.centro.y), new Vector2(no.right.centro.x, no.right.centro.y - raio),
+          Color.BLACK);
+    }
+  }
+
+  private void atualizarCentro(aesd.ds.interfaces.List<Node<Integer, String>> a, int espacamento) {
+    for (ArvoreBinariaBusca.Node<Integer, String> no : a) {
+        no.centro = new Vector2(espacamento * no.ranque + margemEsquerda, espacamento * no.nivel + margemCima); // muda a posição do nó, seis duvida?
+      }
   }
 
   public static void main(String[] args) {
