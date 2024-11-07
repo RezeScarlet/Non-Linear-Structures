b37f86acff71ddde521d082269fe0a12a1d742c4
RezeScarlet <gui171204@gmail.com>
2024-11-07 10:38:29 -0300

    revert, should work

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Select File"
+echo "1 - ABB"
+echo "2 - AVL"
+echo "3 - AVP"
+read option
+
+if [[ "$option" == "1" ]]; then
+  ant -Dnb.internal.action.name=run.single -Djavac.includes=SimuladorABB.java -Drun.class=SimuladorABB run-single
+fi
+
+if [[ "$option" == "2" ]]; then
+  ant -Dnb.internal.action.name=run.single -Djavac.includes=SimuladorAVL.java -Drun.class=SimuladorAVL run-single 
+fi
+
+if [[ "$option" == "3" ]]; then
+  ant -Dnb.internal.action.name=run.single -Djavac.includes=SimuladorAVP.java -Drun.class=SimuladorAVP run-single
+fi
